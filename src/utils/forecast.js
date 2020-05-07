c5a1a724e1987366c2e89f2beb36edbdd6cc87f8
yogikatba <yogikatba16@gmail.com>
2020-05-07 19:30:47 +0530

    Add more details in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lati, long, callback)=>{
             callback('Unable to find location')        
         }
         else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. There is ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. There is ' + body.current.precip + '% chance of rain. The humidity is ' + body.current.humidity + '%.')
         }    
     })
 }
